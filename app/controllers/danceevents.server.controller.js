5613c1def4f9261306ec6ea1fabe8f0bd282c4e1
Ray <greatnightus@gmail.com>
2015-10-14 11:59:14 -0400

    Image upload working

--- app/controllers/danceevents.server.controller.js
+++ app/controllers/danceevents.server.controller.js
@@ -6,6 +6,7 @@
 var mongoose = require('mongoose'),
 	errorHandler = require('./errors.server.controller'),
 	Danceevent = mongoose.model('Danceevent'),
+    Upload = require('s3-uploader'),
 	_ = require('lodash');
 
 
@@ -14,16 +15,70 @@ var mongoose = require('mongoose'),
  */
 exports.create = function(req, res) {
 
-	//req.body.flyer = req.file.url;
+
+	 //todo : store file to disk n the server and then use node-s3-uploader.
+	//because with s3-multer we cannot set the rights / ACL during the upload. such a shame.
+	/* todo for later use.
+	* var client = new Upload('my_s3_bucket', {
+	 aws: {
+	 path: 'images/',
+	 region: 'us-east-1',
+	 acl: 'public-read'
+	 },
+
+	 cleanup: {
+	 versions: true,
+	 original: false
+	 },
+
+	 original: {
+	 awsImageAcl: 'private'
+	 },
+
+	 versions: [{
+	 maxHeight: 1040,
+	 maxWidth: 1040,
+	 format: 'jpg',
+	 suffix: '-large',
+	 quality: 80
+	 },{
+	 maxWidth: 780,
+	 aspect: '3:2!h',
+	 suffix: '-medium'
+	 },{
+	 maxWidth: 320,
+	 aspect: '16:9!h',
+	 suffix: '-small'
+	 },{
+	 maxHeight: 100,
+	 aspect: '1:1',
+	 format: 'png',
+	 suffix: '-thumb1'
+	 },{
+	 maxHeight: 250,
+	 maxWidth: 250,
+	 aspect: '1:1',
+	 suffix: '-thumb2'
+	 }]
+	 });
+
+
+	 client.upload('/some/image.jpg', {}, function(err, versions, meta) {
+	 if (err) { throw err; }
+
+	 versions.forEach(function(image) {
+	 console.log(image.width, image.height, image.url);
+	 // 1234 4567 https://my-bucket.s3.amazonaws.com/path/ab/cd/ef.jpg
+	 });
+	 });
+	* */
+	if(_.get(req,'file.fieldname') === 'flyer'){
+		req.body.flyer = 'https://s3.amazonaws.com/danceappprod/' + _.get(req,'file.key','');
+	}else{
+		req.body.flyer = '';
+	}
 
 	var danceevent = new Danceevent(req.body);
-
-//	if(_.get(req,'file.fieldname') === 'flyer'){
-		body.flyer = 'https://s3.amazonaws.com/danceappprod/' + req.file.key; /*_.get(req,'file.key','');
-*///	}else{
-//		body.flyer = '';
-//	}
-
 
 	danceevent.user = req.user;
 
